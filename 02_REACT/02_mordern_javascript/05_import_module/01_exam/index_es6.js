f51f2b95f5edd3dbbd963db6a9115a1dcfea0b5c
NHH7209 <131868012+NHH7209@users.noreply.github.com>
2023-09-06 10:05:48 +0900

    2st commit

---
+++ 02_REACT/02_mordern_javascript/05_import_module/01_exam/index_es6.js
@@ -0,0 +1,16 @@
+// index_es6.js
+// TODO: num 이 3의 배수인지 판단해서 가져온 변수를 이용해서 출려하세요
+// 입력 : x
+// 출력 : 3의 배수입니다.
+// TODO: import 하세요
+import {three, notThree} from "./var_es6.js";
+
+// 초기값
+let num = 3;
+
+// 
+if(num % 3 == 0){
+    console.log(three); // 3의 배수입니다. 출력
+}else{
+    console.log(notThree); // 3의 배수가 아닙니다. 출력
+}
