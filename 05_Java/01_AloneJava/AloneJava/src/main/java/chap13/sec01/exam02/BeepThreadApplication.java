3ea898a4957306ae2dad84c1ea0aba0d1fc16f8e
NHH7209 <131868012+NHH7209@users.noreply.github.com>
2023-10-02 17:42:13 +0900

    10.02.Java

---
+++ 05_Java/01_AloneJava/AloneJava/src/main/java/chap13/sec01/exam02/BeepThreadApplication.java
@@ -0,0 +1,17 @@
+package chap13.sec01.exam02;
+
+/**
+ * packageName : chap13.sec01.exam02
+ * fileName : BeepThreadApplication
+ * author : GGG
+ * date : 2023-10-02
+ * description : 2) Thread 클래스를 상속해서 구현하기
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-10-02         GGG          최초 생성
+ */
+public class BeepThreadApplication {
+}
