b64603917e86d2f3e541f612c3286c31350825b2
NHH7209 <131868012+NHH7209@users.noreply.github.com>
2023-10-20 17:48:09 +0900

    10.20.React-DB

--- 07_SI/01_SimpleDMS/SimpleDMS/src/main/java/com/example/simpledms/repository/EmpRepository.java
+++ 07_SI/01_SimpleDMS/SimpleDMS/src/main/java/com/example/simpledms/repository/EmpRepository.java
@@ -1,27 +1,24 @@
 package com.example.simpledms.repository;
 
-import com.example.simpledms.model.Dept;
 import com.example.simpledms.model.Emp;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * packageName : com.example.simpledms.repository
- * fileName : DeptRepository
+ * fileName : EmpRepository
  * author : GGG
- * date : 2023-10-19
- * description : JPA 레포 (CRUD)
+ * date : 2023-10-20
+ * description : Emp 레포(CRUD)
  * 요약 :
  * <p>
  * ===========================================================
  * DATE            AUTHOR             NOTE
  * —————————————————————————————
- * 2023-10-19         GGG          최초 생성
+ * 2023-10-20         GGG          최초 생성
  */
-@Repository
-public interface EmpRepository extends JpaRepository<Emp, Integer> {
-    //    Emp like 검색
+public interface EmpRepository extends JpaRepository<Emp,Integer> {
+    //    ename like 함수
     List<Emp> findAllByEnameContaining(String ename);
 }
